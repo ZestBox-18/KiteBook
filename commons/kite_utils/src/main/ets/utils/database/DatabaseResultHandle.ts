49bd4d0d239c4f2514020650d82f2b5a9fd7b21d
ZestBox <zestbox@outlook.com>
2026-05-20 23:06:27 +0800

    refactor: 调整数据库查询调试日志

--- commons/kite_utils/src/main/ets/utils/database/DatabaseResultHandle.ts
+++ commons/kite_utils/src/main/ets/utils/database/DatabaseResultHandle.ts
@@ -2,7 +2,6 @@ import { relationalStore } from "@kit.ArkData";
 
 export function resultSet2Record(resultSet: relationalStore.ResultSet): Record<string, any>[] {
   let res: Record<string, any>[] = []
-  console.log(resultSet.rowCount.toString())
   resultSet.goToFirstRow()
   for (let index = 0; index < resultSet.rowCount; index++) {
     res.push(resultSet.getRow() as Record<string, any>)
